5ddf735fbb06547a353895d425e3fdf9eb61174b
TuanDao2002 <s3877347@rmit.edu.vn>
2022-12-14 17:50:32 +0700

    remove redundant codes

--- app.js
+++ app.js
@@ -36,14 +36,6 @@ app.use(useragent.express());
 app.use(express.json());
 app.use(cookieParser());
 
-// config cloudinary V2
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 // routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,11 +14,8 @@ const {
     resetPassword,
 } = require("../controllers/authController");
 
-const { uploadUserImage } = require("../controllers/imageController");
-
 router.post("/register", register);
 router.post("/verify-email", verifyEmail);
-router.post("/upload-image", uploadUserImage);
 router.post("/login", login);
 router.delete("/logout", authenticateUser, logout);
 router.post("/forgot-password", forgotPassword);
