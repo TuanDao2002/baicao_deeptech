eb0dca39e7eb461f27751c4e2837a0f56bd54cd2
TuanDao2002 <tuan09092002@gmail.com>
2022-12-15 23:04:36 +0700

    add view room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -6,115 +6,153 @@ const Room = require("../models/Room");
 const { generateCards } = require("../utils");
 
 const getRooms = async (req, res) => {
-    let {
-        query: { next_cursor },
-    } = req;
+	let {
+		query: { next_cursor },
+	} = req;
 
-    const queryObject = {};
+	const queryObject = {};
 
-    const resultsLimitPerLoading = 10;
-    if (next_cursor) {
-        const [createdAt, _id] = Buffer.from(next_cursor, "base64")
-            .toString("ascii")
-            .split("_");
+	const resultsLimitPerLoading = 10;
+	if (next_cursor) {
+		const [createdAt, _id] = Buffer.from(next_cursor, "base64")
+			.toString("ascii")
+			.split("_");
 
-        queryObject.createdAt = { $lte: createdAt };
-        queryObject._id = { $lt: _id };
-    }
+		queryObject.createdAt = { $lte: createdAt };
+		queryObject._id = { $lt: _id };
+	}
 
-    let rooms = Room.find(queryObject)
-        .populate({
-            path: "players",
-            select: "-email -password",
-        })
-        .populate({
-            path: "dealer",
-            select: "-email -password",
-        });
+	let rooms = Room.find(queryObject, { players: 0, cards: 0 }).populate({
+		path: "dealer",
+		select: "-email -password",
+	});
 
-    rooms = rooms.sort("-createdAt -_id");
-    rooms = rooms.limit(resultsLimitPerLoading);
-    const results = await rooms;
+	rooms = rooms.sort("-createdAt -_id");
+	rooms = rooms.limit(resultsLimitPerLoading);
+	const results = await rooms;
 
-    const count = await Room.countDocuments(queryObject);
-    next_cursor = null;
+	const count = await Room.countDocuments(queryObject);
+	next_cursor = null;
 
-    // if the there are still remaining results, create a cursor to load the next ones
-    if (count !== results.length) {
-        const lastResult = results[results.length - 1];
-        next_cursor = Buffer.from(
-            lastResult.createdAt.toISOString() + "_" + lastResult._id
-        ).toString("base64");
-    }
+	// if the there are still remaining results, create a cursor to load the next ones
+	if (count !== results.length) {
+		const lastResult = results[results.length - 1];
+		next_cursor = Buffer.from(
+			lastResult.createdAt.toISOString() + "_" + lastResult._id
+		).toString("base64");
+	}
 
-    res.status(StatusCodes.OK).json({ results, next_cursor });
+	res.status(StatusCodes.OK).json({ results, next_cursor });
+};
+
+const viewRoom = async (req, res) => {
+	let {
+		params: { roomId },
+	} = req;
+
+	const room = await Room.findOne({ _id: roomId })
+		.populate({
+			path: "dealer",
+			select: "-email -password",
+		})
+		.populate({
+			path: "players",
+			select: "-email -password",
+		});
+
+	if (!room) {
+		throw new CustomError.BadRequestError("This room does not exist");
+	}
+
+	res.status(StatusCodes.OK).json({ room });
 };
 
 const createRoom = async (req, res) => {
-    let {
-        user: { userId },
-    } = req;
+	let {
+		user: { userId },
+	} = req;
 
-    const newRoom = {
-        dealer: userId,
-        cards: generateCards,
-    };
+	const newRoom = {
+		dealer: userId,
+		cards: generateCards,
+	};
 
-    const room = await Room.create(newRoom);
-    res.status(StatusCodes.OK).json({ room });
+	const room = await Room.create(newRoom);
+	res.status(StatusCodes.OK).json({ room });
+};
+
+const generateShareLink = async (req, res) => {
+	let {
+		params: { roomId },
+	} = req;
+
+	const room = await Room.findOne({ _id: roomId });
+	if (!room) {
+		throw new CustomError.BadRequestError("This room does not exist");
+	}
+
+	res.status(StatusCodes.OK).json({
+		shareLink:
+			req.protocol + "://" + req.get("host") + "/api/room/join/" + roomId,
+	});
 };
 
 const joinRoom = async (req, res) => {
-    let {
-        user: { userId },
-        params: { roomId },
-    } = req;
+	let {
+		user: { userId },
+		params: { roomId },
+	} = req;
 
-    const room = await Room.findOne({ _id: roomId });
-    if (!room) {
-        throw new CustomError.BadRequestError("This room does not exist");
-    }
+	const room = await Room.findOne({ _id: roomId });
+	if (!room) {
+		throw new CustomError.BadRequestError("This room does not exist");
+	}
 
-    const { dealer, players, isFull } = room;
-    if (isFull) throw new CustomError.BadRequestError("This room is full");
+	const { dealer, players, isFull } = room;
+	if (isFull) throw new CustomError.BadRequestError("This room is full");
 
-    if (dealer.toString() === userId) {
-        throw new CustomError.BadRequestError(
-            "You are already a dealer in this room"
-        );
-    }
+	if (dealer.toString() === userId) {
+		throw new CustomError.BadRequestError(
+			"You are already a dealer in this room"
+		);
+	}
 
-    if (players.includes(userId)) {
-        throw new CustomError.BadRequestError("You already joined this room");
-    }
+	if (players.includes(userId)) {
+		throw new CustomError.BadRequestError("You already joined this room");
+	}
 
-    players.push(userId);
-    room.players = players;
+	players.push(userId);
+	room.players = players;
+	if (room.players.length === 3) {
+		room.isFull = true;
+	}
 
-    await room.save();
-    res.status(StatusCodes.OK).json({ room });
+	await room.save();
+	res.status(StatusCodes.OK).json({ room });
 };
 
 const leaveRoom = async (req, res) => {
-    let {
-        user: { userId },
-        params: { roomId },
-    } = req;
+	let {
+		user: { userId },
+		params: { roomId },
+	} = req;
 
-    const room = await Room.findOne({ _id: roomId });
-    if (!room) {
-        throw new CustomError.BadRequestError("This room does not exist");
-    }
+	const room = await Room.findOne({ _id: roomId });
+	if (!room) {
+		throw new CustomError.BadRequestError("This room does not exist");
+	}
 
-    const { dealer, players, isFull } = room;
-    if (!players.includes(userId)) {
-        throw new CustomError.BadRequestError("You have not joined this room");
-    }
+	const { dealer, players, isFull } = room;
+	if (!players.includes(userId)) {
+		throw new CustomError.BadRequestError("You have not joined this room");
+	}
 };
 
 module.exports = {
-    getRooms,
-    createRoom,
-    joinRoom,
-    leaveRoom,
+	getRooms,
+    viewRoom,
+	createRoom,
+	generateShareLink,
+	joinRoom,
+	leaveRoom,
 };
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -4,14 +4,18 @@ const router = express.Router();
 const { authenticateUser } = require("../middleware/authentication");
 
 const {
-    getRooms,
-    createRoom,
-    joinRoom,
-    leaveRoom,
+	getRooms,
+	viewRoom,
+	createRoom,
+	generateShareLink,
+	joinRoom,
+	leaveRoom,
 } = require("../controllers/roomController");
 
 router.get("/all", getRooms);
+router.get("/view/:roomId", viewRoom);
 router.post("/create", authenticateUser, createRoom);
+router.post("/share/:roomId", generateShareLink);
 router.patch("/join/:roomId", authenticateUser, joinRoom);
 router.patch("/leave", authenticateUser, leaveRoom);
 
