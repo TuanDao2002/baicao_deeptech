fa05d3797abde40d756a71d7b37eb8250cf65df1
TuanDao2002 <s3877347@rmit.edu.vn>
2022-12-17 22:50:12 +0700

    hand added or removed after player joined or leaved the room

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -9,172 +9,166 @@ const { changeDealer, shuffle, allCardsInDeck, constant } = require("../utils");
 const mongoose = require("mongoose");
 const { handle } = require("express/lib/router");
 const {
-	MAX_NUM_CARDS_IN_DECK,
-	MAX_NUM_CARDS_IN_HAND,
-	MAX_NUM_PLAYERS,
-	DEFAULT_COINS,
-	DEDUCT_COINS,
+    MAX_NUM_CARDS_IN_DECK,
+    MAX_NUM_CARDS_IN_HAND,
+    MAX_NUM_PLAYERS,
+    DEFAULT_COINS,
+    DEDUCT_COINS,
 } = constant;
 
 const shuffleDeck = async (req, res) => {
-	let {
-		user: { userId },
-		params: { deckId },
-	} = req;
+    let {
+        user: { userId },
+        params: { deckId },
+    } = req;
 
-	const findRoom = await Room.findOne({ dealer: userId, deck: deckId });
-	if (!findRoom) {
-		throw new CustomError.BadRequestError(
-			"You are not the dealer of this room"
-		);
-	}
+    const findRoom = await Room.findOne({ dealer: userId, deck: deckId });
+    if (!findRoom) {
+        throw new CustomError.BadRequestError(
+            "You are not the dealer of this room"
+        );
+    }
 
-	let deck = await Deck.findOne({ _id: deckId }).populate({
-		path: "hands.player",
-		select: "-email -password",
-	});
+    let deck = await Deck.findOne({ _id: deckId }).populate({
+        path: "hands.player",
+        select: "-email -password",
+    });
 
-	if (!deck) {
-		throw new CustomError.BadRequestError("This deck does not exist");
-	}
+    if (!deck) {
+        throw new CustomError.BadRequestError("This deck does not exist");
+    }
 
-	deck.remainingCards = shuffle(allCardsInDeck);
-	deck.remaining = MAX_NUM_CARDS_IN_DECK;
-	for (let hand of deck.playerHands) {
-		if (findRoom.players.includes(mongoose.Types.ObjectId(hand.player))) {
-			hand.cards = [];
-			hand.point = 0;
-		} else {
-			deck.playerHands.remove(hand);
-		}
-	}
+    deck.remainingCards = shuffle(allCardsInDeck);
+    deck.remaining = MAX_NUM_CARDS_IN_DECK;
+    for (let hand of deck.playerHands) {
+        hand.cards = [];
+        hand.point = 0;
+    }
 
-	if (deck.dealerHand) {
-		deck.dealerHand.player = findRoom.dealer;
-		deck.dealerHand.cards = [];
-		deck.dealerHand.point = 0;
-	}
+    if (deck.dealerHand) {
+        deck.dealerHand.cards = [];
+        deck.dealerHand.point = 0;
+    }
 
-	deck = await deck.save();
-	deck = await deck
-		.populate({
-			path: "dealerHand.player",
-			select: "-email -password",
-		})
-		.populate({
-			path: "playerHands.player",
-			select: "-email -password",
-		})
-		.execPopulate();
+    deck = await deck.save();
+    deck = await deck
+        .populate({
+            path: "dealerHand.player",
+            select: "-email -password",
+        })
+        .populate({
+            path: "playerHands.player",
+            select: "-email -password",
+        })
+        .execPopulate();
 
-	res.status(StatusCodes.OK).json({ deck });
+    res.status(StatusCodes.OK).json({ deck });
 };
 
 const dealCards = (deck, id, isDealer) => {
-	const cards = deck.remainingCards.splice(0, MAX_NUM_CARDS_IN_HAND);
-	let totalPoints = 0;
-	for (let card of cards) {
-		const [rank, suite] = card.split(" of ");
-		if (specialRanks.includes(rank)) {
-			if (rank === "A") {
-				totalPoints += 1;
-			} else {
-				totalPoints += 10;
-			}
-		} else {
-			totalPoints += parseInt(rank);
-		}
-	}
+    const cards = deck.remainingCards.splice(0, MAX_NUM_CARDS_IN_HAND);
+    let totalPoints = 0;
+    for (let card of cards) {
+        const [rank, suite] = card.split(" of ");
+        if (specialRanks.includes(rank)) {
+            if (rank === "A") {
+                totalPoints += 1;
+            } else {
+                totalPoints += 10;
+            }
+        } else {
+            totalPoints += parseInt(rank);
+        }
+    }
 
-	if (isDealer && deck.dealerHand) {
-		deck.dealerHand.player = id;
-		deck.dealerHand.cards = cards;
-		deck.dealerHand.point = totalPoints % 10;
-		deck.remaining -= MAX_NUM_CARDS_IN_HAND;
+    if (isDealer && deck.dealerHand) {
+        deck.dealerHand.cards = cards;
+        deck.dealerHand.point = totalPoints % 10;
+        deck.remaining -= MAX_NUM_CARDS_IN_HAND;
 
-		return deck;
-	}
+        return deck;
+    }
 
-	for (let hand of deck.playerHands) {
-		if (!isDealer && hand.player.equals(id)) {
-			hand.cards = cards;
-			hand.point = totalPoints % 10;
-			deck.remaining -= MAX_NUM_CARDS_IN_HAND;
+    for (let hand of deck.playerHands) {
+        if (!isDealer && hand.player.equals(id)) {
+            hand.cards = cards;
+            hand.point = totalPoints % 10;
+            deck.remaining -= MAX_NUM_CARDS_IN_HAND;
 
-			return deck;
-		}
-	}
+            return deck;
+        }
+    }
 
-	const hand = {
-		player: id,
-		cards: cards,
-		point: totalPoints % 10,
-		coins: DEFAULT_COINS,
-	};
+    // const hand = {
+    //     player: id,
+    //     cards: cards,
+    //     point: totalPoints % 10,
+    //     coins: DEFAULT_COINS,
+    // };
 
-	deck.remaining -= MAX_NUM_CARDS_IN_HAND;
+    // deck.remaining -= MAX_NUM_CARDS_IN_HAND;
 
-	if (isDealer) {
-		deck.dealerHand = hand;
-	} else {
-		deck.playerHands.push(hand);
-	}
+    // if (isDealer) {
+    //     deck.dealerHand = hand;
+    // } else {
+    //     deck.playerHands.push(hand);
+    // }
 
-	return deck;
+    return deck;
 };
 
 const drawn = async (req, res) => {
-	let {
-		user: { userId },
-		params: { deckId },
-	} = req;
+    let {
+        user: { userId },
+        params: { deckId },
+    } = req;
 
-	const room = await Room.findOne({ dealer: userId, deck: deckId });
-	if (!room) {
-		throw new CustomError.BadRequestError(
-			"You are not the dealer of this room"
-		);
-	}
+    const room = await Room.findOne({ dealer: userId, deck: deckId });
+    if (!room) {
+        throw new CustomError.BadRequestError(
+            "You are not the dealer of this room"
+        );
+    }
 
-	let deck = await Deck.findOne({ _id: deckId });
+    let deck = await Deck.findOne({ _id: deckId });
 
-	if (!deck) {
-		throw new CustomError.BadRequestError("This deck does not exist");
-	}
+    if (!deck) {
+        throw new CustomError.BadRequestError("This deck does not exist");
+    }
 
-	const { players } = room;
-	let numOfPlayers = players.length + 1;
-	if (deck.remaining < numOfPlayers * MAX_NUM_CARDS_IN_HAND) {
-		throw new CustomError.BadRequestError(
-			"Not enough cards for all players, please shuffle again"
-		);
-	}
+    const { players } = room;
+    let numOfPlayers = players.length + 1;
+    if (deck.remaining < numOfPlayers * MAX_NUM_CARDS_IN_HAND) {
+        throw new CustomError.BadRequestError(
+            "Not enough cards for all players, please shuffle again"
+        );
+    }
 
-	if (numOfPlayers === 1) {
-		throw new CustomError.BadRequestError(
-			"Must have at least 2 players in the room to play"
-		);
-	}
+    if (numOfPlayers === 1) {
+        throw new CustomError.BadRequestError(
+            "Must have at least 2 players in the room to play"
+        );
+    }
 
-	for (let player of players) {
-		deck = dealCards(deck, player, false);
-	}
+    for (let player of players) {
+        deck = dealCards(deck, player, false);
+    }
 
-	deck = dealCards(deck, userId, true);
+    deck = dealCards(deck, userId, true);
 
-	deck = await deck.save();
-	deck = await deck
-		.populate({
-			path: "dealerHand.player",
-			select: "-email -password",
-		})
-		.populate({
-			path: "playerHands.player",
-			select: "-email -password",
-		})
-		.execPopulate();
+    deck = await deck.save();
+    deck = await deck
+        .populate({
+            path: "dealerHand.player",
+            select: "-email -password",
+        })
+        .populate({
+            path: "playerHands.player",
+            select: "-email -password",
+        })
+        .execPopulate();
 
-	res.status(StatusCodes.OK).json({ deck });
+    res.status(StatusCodes.OK).json({ deck });
 };
 
 const reveal = async (req, res) => {};
@@ -182,8 +176,8 @@ const reveal = async (req, res) => {};
 const reset = async (req, res) => {};
 
 module.exports = {
-	shuffleDeck,
-	drawn,
-	reveal,
-	reset,
+    shuffleDeck,
+    drawn,
+    reveal,
+    reset,
 };
--- controllers/roomController.js
+++ controllers/roomController.js
@@ -9,242 +9,267 @@ const mongoose = require("mongoose");
 const MAX_NUM_PLAYERS = constant.MAX_NUM_PLAYERS;
 
 const getRooms = async (req, res) => {
-	let {
-		query: { next_cursor },
-	} = req;
+    let {
+        query: { next_cursor },
+    } = req;
 
-	const queryObject = {};
+    const queryObject = {};
 
-	const resultsLimitPerLoading = 10;
-	if (next_cursor) {
-		const [createdAt, _id] = Buffer.from(next_cursor, "base64")
-			.toString("ascii")
-			.split("_");
+    const resultsLimitPerLoading = 10;
+    if (next_cursor) {
+        const [createdAt, _id] = Buffer.from(next_cursor, "base64")
+            .toString("ascii")
+            .split("_");
 
-		queryObject.createdAt = { $lte: createdAt };
-		queryObject._id = { $lt: _id };
-	}
+        queryObject.createdAt = { $lte: createdAt };
+        queryObject._id = { $lt: _id };
+    }
 
-	let rooms = Room.find(queryObject, { players: 0, cards: 0 }).populate({
-		path: "dealer",
-		select: "-email -password",
-	});
+    let rooms = Room.find(queryObject, { players: 0, cards: 0 }).populate({
+        path: "dealer",
+        select: "-email -password",
+    });
 
-	rooms = rooms.sort("-createdAt -_id");
-	rooms = rooms.limit(resultsLimitPerLoading);
-	const results = await rooms;
+    rooms = rooms.sort("-createdAt -_id");
+    rooms = rooms.limit(resultsLimitPerLoading);
+    const results = await rooms;
 
-	const count = await Room.countDocuments(queryObject);
-	next_cursor = null;
+    const count = await Room.countDocuments(queryObject);
+    next_cursor = null;
 
-	// if the there are still remaining results, create a cursor to load the next ones
-	if (count !== results.length) {
-		const lastResult = results[results.length - 1];
-		next_cursor = Buffer.from(
-			lastResult.createdAt.toISOString() + "_" + lastResult._id
-		).toString("base64");
-	}
+    // if the there are still remaining results, create a cursor to load the next ones
+    if (count !== results.length) {
+        const lastResult = results[results.length - 1];
+        next_cursor = Buffer.from(
+            lastResult.createdAt.toISOString() + "_" + lastResult._id
+        ).toString("base64");
+    }
 
-	res.status(StatusCodes.OK).json({ results, next_cursor });
+    res.status(StatusCodes.OK).json({ results, next_cursor });
 };
 
 const viewRoom = async (req, res) => {
-	let {
-		params: { roomId },
-	} = req;
+    let {
+        params: { roomId },
+    } = req;
 
-	const room = await Room.findOne({ _id: roomId })
-		.populate({
-			path: "dealer",
-			select: "-email -password",
-		})
-		.populate({
-			path: "players",
-			select: "-email -password",
-		});
+    const room = await Room.findOne({ _id: roomId })
+        .populate({
+            path: "dealer",
+            select: "-email -password",
+        })
+        .populate({
+            path: "players",
+            select: "-email -password",
+        });
 
-	if (!room) {
-		throw new CustomError.BadRequestError("This room does not exist");
-	}
+    if (!room) {
+        throw new CustomError.BadRequestError("This room does not exist");
+    }
 
-	res.status(StatusCodes.OK).json({ room });
+    res.status(StatusCodes.OK).json({ room });
 };
 
 const createRoom = async (req, res) => {
-	let {
-		user: { userId },
-	} = req;
+    let {
+        user: { userId },
+    } = req;
 
-	const findRoom = await Room.findOne({
-		$or: [{ dealer: userId }, { players: userId }],
-	});
+    const findRoom = await Room.findOne({
+        $or: [{ dealer: userId }, { players: userId }],
+    });
 
-	if (findRoom) {
-		if (findRoom.dealer.toString() === userId) {
-			throw new CustomError.BadRequestError(
-				"You are already a dealer in another room"
-			);
-		}
+    if (findRoom) {
+        if (findRoom.dealer.toString() === userId) {
+            throw new CustomError.BadRequestError(
+                "You are already a dealer in another room"
+            );
+        }
 
-		if (findRoom.players.includes(mongoose.Types.ObjectId(userId))) {
-			throw new CustomError.BadRequestError(
-				"You are already a player in another room"
-			);
-		}
-	}
+        if (findRoom.players.includes(mongoose.Types.ObjectId(userId))) {
+            throw new CustomError.BadRequestError(
+                "You are already a player in another room"
+            );
+        }
+    }
 
-	const deck = await Deck.create({ remainingCards: shuffle(allCardsInDeck) });
-	const newRoom = {
-		dealer: userId,
-		deck: deck._id,
-	};
+    const deck = await Deck.create({
+        remainingCards: shuffle(allCardsInDeck),
+        dealerHand: {
+            player: userId,
+        },
+    });
 
-	const room = await Room.create(newRoom);
-	res.status(StatusCodes.OK).json({ room });
+    const newRoom = {
+        dealer: userId,
+        deck: deck._id,
+    };
+
+    const room = await Room.create(newRoom);
+    res.status(StatusCodes.OK).json({ room });
 };
 
 const generateShareLink = async (req, res) => {
-	let {
-		params: { roomId },
-	} = req;
+    let {
+        params: { roomId },
+    } = req;
 
-	const room = await Room.findOne({ _id: roomId });
-	if (!room) {
-		throw new CustomError.BadRequestError("This room does not exist");
-	}
+    const room = await Room.findOne({ _id: roomId });
+    if (!room) {
+        throw new CustomError.BadRequestError("This room does not exist");
+    }
 
-	res.status(StatusCodes.OK).json({
-		shareLink:
-			req.protocol + "://" + req.get("host") + "/api/room/view/" + roomId,
-	});
+    res.status(StatusCodes.OK).json({
+        shareLink:
+            req.protocol + "://" + req.get("host") + "/api/room/view/" + roomId,
+    });
 };
 
 const joinRoom = async (req, res) => {
-	let {
-		user: { userId },
-		params: { roomId },
-	} = req;
+    let {
+        user: { userId },
+        params: { roomId },
+    } = req;
 
-	let room = await Room.findOne({ _id: roomId });
-	if (!room) {
-		throw new CustomError.BadRequestError("This room does not exist");
-	}
+    let room = await Room.findOne({ _id: roomId });
+    if (!room) {
+        throw new CustomError.BadRequestError("This room does not exist");
+    }
 
-	const { dealer, players, isFull } = room;
-	if (isFull) throw new CustomError.BadRequestError("This room is full");
+    const { dealer, players, deck, isFull } = room;
+    if (isFull) throw new CustomError.BadRequestError("This room is full");
 
-	if (dealer.toString() === userId) {
-		throw new CustomError.BadRequestError(
-			"You are already a dealer in this room"
-		);
-	}
+    if (dealer.toString() === userId) {
+        throw new CustomError.BadRequestError(
+            "You are already a dealer in this room"
+        );
+    }
 
-	if (players.includes(mongoose.Types.ObjectId(userId))) {
-		throw new CustomError.BadRequestError("You already joined this room");
-	}
+    if (players.includes(mongoose.Types.ObjectId(userId))) {
+        throw new CustomError.BadRequestError("You already joined this room");
+    }
 
-	const findRoom = await Room.findOne({
-		$or: [{ dealer: userId }, { players: userId }],
-	});
+    const findRoom = await Room.findOne({
+        $or: [{ dealer: userId }, { players: userId }],
+    });
 
-	if (findRoom) {
-		if (findRoom.dealer.toString() === userId) {
-			throw new CustomError.BadRequestError(
-				"You are already a dealer in another room"
-			);
-		}
+    if (findRoom) {
+        if (findRoom.dealer.toString() === userId) {
+            throw new CustomError.BadRequestError(
+                "You are already a dealer in another room"
+            );
+        }
 
-		if (findRoom.players.includes(mongoose.Types.ObjectId(userId))) {
-			throw new CustomError.BadRequestError(
-				"You are already a player in another room"
-			);
-		}
-	}
+        if (findRoom.players.includes(mongoose.Types.ObjectId(userId))) {
+            throw new CustomError.BadRequestError(
+                "You are already a player in another room"
+            );
+        }
+    }
 
-	players.push(userId);
-	room.players = players;
-	if (room.players.length >= MAX_NUM_PLAYERS) {
-		room.isFull = true;
-	}
+    const findDeck = await Deck.findOne({ _id: deck });
+    findDeck.playerHands.push({
+        player: userId,
+    });
+    await findDeck.save();
 
-	room = await room.save();
-	room = await room
-		.populate({
-			path: "players",
-			select: "-email -password",
-		})
-		.populate({
-			path: "dealer",
-			select: "-email -password",
-		})
-		.execPopulate();
+    players.push(userId);
+    room.players = players;
+    if (room.players.length >= MAX_NUM_PLAYERS) {
+        room.isFull = true;
+    }
 
-	res.status(StatusCodes.OK).json({ room });
+    room = await room.save();
+    room = await room
+        .populate({
+            path: "players",
+            select: "-email -password",
+        })
+        .populate({
+            path: "dealer",
+            select: "-email -password",
+        })
+        .execPopulate();
+
+    res.status(StatusCodes.OK).json({ room });
 };
 
 const leaveRoom = async (req, res) => {
-	let {
-		user: { userId },
-		params: { roomId },
-	} = req;
+    let {
+        user: { userId },
+        params: { roomId },
+    } = req;
 
-	let room = await Room.findOne({ _id: roomId }).populate({
-		path: "players",
-		select: "-email -password",
-	});
+    let room = await Room.findOne({ _id: roomId }).populate({
+        path: "players",
+        select: "-email -password",
+    });
 
-	if (!room) {
-		throw new CustomError.BadRequestError("This room does not exist");
-	}
+    if (!room) {
+        throw new CustomError.BadRequestError("This room does not exist");
+    }
 
-	const { dealer, players } = room;
-	let isPlayer = false;
-	for (let player of players) {
-		if (player._id.toString() === userId) {
-			isPlayer = true;
-			break;
-		}
-	}
+    const { dealer, players, deck } = room;
+    let isPlayer = false;
+    for (let player of players) {
+        if (player._id.toString() === userId) {
+            isPlayer = true;
+            break;
+        }
+    }
 
-	if (!isPlayer && dealer.toString() !== userId) {
-		throw new CustomError.BadRequestError("You have not joined this room");
-	}
+    if (!isPlayer && dealer.toString() !== userId) {
+        throw new CustomError.BadRequestError("You have not joined this room");
+    }
 
-	if (dealer.toString() === userId) {
-		if (players.length <= 0) {
-			await room.remove();
-			res.status(StatusCodes.OK).json({ msg: "This room is removed" });
-			return;
-		}
+    const findDeck = await Deck.findOne({ _id: deck });
 
-		room = await changeDealer(room);
-	} else if (isPlayer) {
-		players.remove(mongoose.Types.ObjectId(userId));
-		room.players = players;
+    if (dealer.toString() === userId) {
+        if (players.length <= 0) {
+            await room.remove();
+            res.status(StatusCodes.OK).json({ msg: "This room is removed" });
+            return;
+        }
 
-		const deck = await Deck.findOne({ _id: room.deck });
-		for (let hand of deck.playerHands) {
-			if (hand.player.equals(userId)) {
-				deck.playerHands.remove(hand);
-			}
-		}
+        room = await changeDealer(room);
 
-		await deck.save();
-	}
+        for (let hand of findDeck.playerHands) {
+            if (hand.player.equals(room.dealer._id)) {
+                findDeck.dealerHand = hand
+                findDeck.playerHands.remove(hand);
 
-	if (room.players.length < MAX_NUM_PLAYERS) {
-		room.isFull = false;
-	}
+                console.log(findDeck);
 
-	await room.save();
-	res.status(StatusCodes.OK).json({ room });
+                await findDeck.save();
+                console.log(findDeck);
+                break;
+            }
+        }
+    } else if (isPlayer) {
+        players.remove(mongoose.Types.ObjectId(userId));
+        room.players = players;
+
+        for (let hand of findDeck.playerHands) {
+            if (hand.player.equals(userId)) {
+                findDeck.playerHands.remove(hand);
+                await findDeck.save();
+            }
+        }
+    }
+
+    if (room.players.length < MAX_NUM_PLAYERS) {
+        room.isFull = false;
+    }
+
+    await room.save();
+    res.status(StatusCodes.OK).json({ room });
 };
 
 module.exports = {
-	getRooms,
-	viewRoom,
-	createRoom,
-	generateShareLink,
-	joinRoom,
-	leaveRoom,
+    getRooms,
+    viewRoom,
+    createRoom,
+    generateShareLink,
+    joinRoom,
+    leaveRoom,
 };
